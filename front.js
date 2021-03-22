e07f3ffa3280e445163f42de4a0f22c28653d1a5
Tanney-102 <dhxodms89@gmail.com>
2021-03-23 00:28:05 +0900

    netlify에서 proxy.js 실행 시 https로 접근 가능한지 실험

--- front.js
+++ front.js
@@ -1,7 +1,7 @@
 console.log("loaded");
 
 document.querySelector("button").addEventListener("click", async () => {
-  const res = await fetch("http://localhost:3001/test");
+  const res = await fetch("https://localhost:3001/test");
   const text = await res.text();
 
   console.log(text);
